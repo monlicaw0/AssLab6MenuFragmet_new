d61a6616fd45fa059f514429d74a3b7b3b3c25fe
monlicaw0 <44289464+monlicaw0@users.noreply.github.com>
2019-08-28 23:15:40 +0700

    commit

---
+++ app/src/main/java/com/myweb/asslab6menufragmet_new/Friend2Fragment.kt
@@ -0,0 +1,42 @@
+package com.myweb.asslab6menufragmet_new
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_friend1.view.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class Friend2Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_friend2, container, false)
+        view.btnBack.setOnClickListener(){
+            var fragment: Fragment? = null
+            fragment = MyFragment()
+            replaceFragment(fragment)
+        }
+        return view
+    }
+    private fun replaceFragment(someFragment: Fragment) {
+        val transaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.frameLayout, someFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
